1e55bdb094771ab5230dcc4dd8e685a9872b45a1
mayer93 <oscar.mayordomo.93@gmail.com>
2022-03-21 22:43:55 +0100

    added spotify player with album selector

---
+++ public/js/scripts.js
@@ -0,0 +1,27 @@
+$(document).on("click", ".spotify-selector-img:not(.selected)", function() {
+    $(".spotify-selector-img.selected").removeClass("selected");
+    $(this).addClass("selected");
+    if ($('#spotify-iframe').length != 0) {
+        $('#spotify-iframe').remove();
+    }
+    var album = $(this).attr('id');
+    var iframeSrc = "";
+    switch (album) {
+        case "true-siberian-icon":
+            iframeSrc = "https://open.spotify.com/embed/album/4LSzNFgbjBP8To1uIe6R12";
+            break;
+        case "reborn-icon":
+            iframeSrc = "https://open.spotify.com/embed/album/2PgcRiV3RN9S1YFuwMOSSJ";
+            break;
+        case "meet-the-doctor-icon":
+            iframeSrc = "https://open.spotify.com/embed/album/4EqShfLsLcZLbmnFac0Fw6";
+            break;
+        default:
+            iframeSrc = "";
+    }
+
+    if (iframeSrc != "") {
+        var elementStr = "<iframe id=\"spotify-iframe\" src='" + iframeSrc + "' width=\"300\" height=\"380\" frameborder='0' allowtransparency='true' allow='encrypted-media'></iframe>";
+        $('.spotify-player').append(elementStr);
+    }
+})
