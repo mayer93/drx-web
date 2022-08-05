876749ccacd04939450be0c1acddc82e371595dd
Oscar <o.mayordomo@taskitservices.com>
2022-08-05 16:33:04 +0200

    fixed fadein/out in spotify player added streaming platforms links added tooltips

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,13 +1,14 @@
 /* select album in spotify player */
 $(document).on("click", ".spotify-selector-img:not(.selected)", function() {
+    /* modify playlist selectors */
     $(".spotify-selector-img.selected").removeClass("selected");
     $(this).addClass("selected");
-    if ($('#spotify-iframe').length != 0) {
-        $('#spotify-iframe').remove();
-        /*$('#spotify-iframe').fadeOut("normal", function() {
-            $(this).remove();
-        })*/
-    }
+    
+    /* retrieve current playlist(s) */
+    var current = $('.spotify-player-inner');
+    var loading = $('.iframe-loading'); /* check if there is any other playlist already loading (for example, if we click two icons in a very short period of time) */
+
+    /* check playlist name and get link to iframe */
     var album = $(this).attr('id');
     var iframeSrc = "";
     switch (album) {
@@ -24,9 +25,20 @@ $(document).on("click", ".spotify-selector-img:not(.selected)", function() {
             iframeSrc = "";
     }
 
+    /* remove current playlist(s) with fadeout and add new one with fadein */
     if (iframeSrc != "") {
-        var elementStr = "<iframe id=\"spotify-iframe\" src='" + iframeSrc + "' width=\"300\" height=\"380\" frameborder='0' allowtransparency='true' allow='encrypted-media'></iframe>";
-        $(elementStr).hide().appendTo('.spotify-player').fadeIn(600);
+        var elementStr = "<div class=\"spotify-player-inner iframe-loading\"><iframe class=\"spotify-iframe\" src='" + iframeSrc + "' width=\"300\" height=\"380\" frameborder='0' allowtransparency='true' allow='encrypted-media'></iframe></div>";
+        $(elementStr).hide().appendTo('.spotify-player').delay(1000).fadeIn(750, function() { /* delay is just for aesthetics (to avoid starting the fadein while loading the playlist) */
+            $(this).removeClass("iframe-loading");
+        });
+        if ($(loading).length != 0) {
+            $(loading).remove(); /* we remove other loading playlists to avoid accumulating them in case we continously click icons */
+        }
+        if ($(current).length != 0) {
+            $(current).fadeOut(1750, function() {
+                $(this).remove();
+            });
+        }
         /*$('.spotify-player').append(elementStr);*/
     }
 })
